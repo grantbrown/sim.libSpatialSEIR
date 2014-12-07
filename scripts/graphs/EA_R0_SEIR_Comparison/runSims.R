62099f495472ae784b4ea11351bec68c7ab91a20
Grant Brown <grant.brown73@gmail.com>
2014-12-07 13:51:56 -0600

    Oops, forgot to write output

--- scripts/graphs/EA_R0_SEIR_Comparison/runSims.R
+++ scripts/graphs/EA_R0_SEIR_Comparison/runSims.R
@@ -9,8 +9,18 @@ dat1 = read.csv("./simR0_1.txt")
 correctSpecItrs = max(dat1$Iteration)
 correctSpecTime = max(dat1$Time)
 
-
 out = matrix(c(underSpecItrs, correctSpecItrs, underSpecTime, correctSpecTime), ncol = 2)
 colnames(out) = c("Iterations", "Time")
+write.csv(out, file="./IterationsTime.csv")
+
+
+#betaDens = density(exp(dat1$BetaP_SE_0[floor(nrow(dat1)/2):nrow(dat1)]))
+#plot(betaDens, 
+#     main = "Intensity Intercept\n Posterior Density", xlab = expression(beta))
+#maxIdx = which.max(betaDens$y)
+#lines(x = rep(betaDens$x[maxIdx],2), c(0, betaDens$y[maxIdx]), lty = 2)
+#density(exp(dat1$BetaP_SE_0[floor(nrow(dat1)/2):nrow(dat1)])
+#text(x=0.25, y = betaDens$y[maxIdx], labels=paste("HPD Estimate: ", 
+#                                                  round(betaDens$x[maxIdx],2),sep=""))
 
 
