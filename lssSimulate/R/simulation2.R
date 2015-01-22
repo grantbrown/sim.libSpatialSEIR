514ed05d1a05ee6d6d4b91d4769d4a04846feda8
Grant Brown <grant.brown73@gmail.com>
2015-01-22 15:47:08 -0600

    Prior should induce some shrinkage

--- lssSimulate/R/simulation2.R
+++ lssSimulate/R/simulation2.R
@@ -111,7 +111,7 @@ buildSingleLocSimInstance2 = function(params)
 
     priorBetaIntercept = log(mean(-log(1-(simResults$I_star/(simResults$N))))) 
     ExposureModel = buildExposureModel(simResults$X, simResults$Z, 
-                                       beta = c(priorBetaIntercept, rep(0, ((length(simResults$beta_SE))-1))), betaPriorPrecision = 0.1)
+                                       beta = c(priorBetaIntercept, rep(0, ((length(simResults$beta_SE))-1))), betaPriorPrecision = 1)
     ReinfectionModel = buildReinfectionModel("SEIR")
     SamplingControl = buildSamplingControl(iterationStride=1000,
                                            sliceWidths = c(0.26,  # S_star
